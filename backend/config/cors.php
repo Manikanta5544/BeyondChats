5e47848f1e8968ae5f5ef4ca46e3d21101f08e11
Manikanta5544 <kasanimanikanta6@gmail.com>
2025-12-24 23:26:00 +0530

    Laravel confix fix

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -1,20 +1,26 @@
 <?php
 
 return [
+
     'paths' => ['api/*'],
-    'allowed_methods' => ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+
+    'allowed_methods' => ['*'],
+
     'allowed_origins' => [
+        'https://beyond-chats-gm6ym8jty-manikanta5544s-projects.vercel.app',
         'https://beyond-chats-ashen.vercel.app',
-        'http://localhost:5173',
-        'http://127.0.0.1:5173',
+        'https://beyond-chats.vercel.app',
     ],
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => [
+        '#^https://.*\.vercel\.app$#'
+    ],
 
-    'allowed_headers' => ['Content-Type', 'Authorization', 'Accept'],
+    'allowed_headers' => ['*'],
 
     'exposed_headers' => [],
 
     'max_age' => 0,
+
     'supports_credentials' => false,
 ];
